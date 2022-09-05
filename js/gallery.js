bfb0825e5ee2c0710f6e8000a8d46de3317c0757
Vitālijs Verhovičs <99669833+teaheey@users.noreply.github.com>
2022-09-05 11:39:53 +0300

    Gallery css and js update to make it more responsive and pretty

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,38 @@
 $( document ).ready(function() {  
-    
+
+    $( ".menu label" ).mouseenter(function() {
+        $(this).css('color', 'white');
+     });
+
+     $( ".menu label" ).mouseleave(function() {
+        $(this).css('color', 'black');
+     });
+
     $( ".menu label" ).click(function() {
+
+       /*
+            making transitions between gallery sections responsive
+       */
         //all photos selected
        if($(this).data('type') == 'all')
        {
-        $('.gallery a').show();
+        $('.menu label').css('text-decoration', 'none');
+        $('.menu label').css('font-size', '15px');
+        $(this).css('text-decoration', 'underline #ff4f5a 3px');
+        $(this).css('font-size', '18px');
+        //show all photos
+        $('.gallery a').fadeOut();
+        $('.gallery a').fadeIn("slow");
        } else {
         //hide all photos
-        $('.gallery a').hide();
+        $('.gallery a').fadeOut("slow");
         //show selected category photos (by class and data-type attributes)
-        $('.gallery').find('.' + $(this).data('type')).show();
-
+        $('.gallery').find('.' + $(this).data('type')).fadeIn("slow");
+        //style selected gallery section
+        $('.menu label').css('text-decoration', 'none');
+        $('.menu label').css('font-size', '15px');
+        $(this).css('text-decoration', 'underline #ff4f5a 3px');
+        $(this).css('font-size', '18px');
        }
 
     });
